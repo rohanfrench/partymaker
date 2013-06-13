95a1ed5b03973ee2f13b953a18a4fe995433a3e5
rohanfrench <rohan.frank21@gmail.com>
2013-06-13 21:00:12 +0000

    basic rails_p created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partymaker::Application.config.secret_token = 'e7522b4aea9858b11925d7a0c735215ace8977a90f750ed8c2e3b30594543b023fcdc8af0372106a6a0d4f5644affe0ee1813261cb1d4ee651367a453118e4c7'
